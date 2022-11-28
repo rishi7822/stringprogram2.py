3bdd3789a9cade7270185111dff4f4df68f0d239
Rushikesh Badgujar <82447941+rishi7822@users.noreply.github.com>
2022-11-29 01:24:18 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,13 @@
+#progrAM TO REMOVE ITH OCCURENCE FROM STARTING
+
+string1 = "rishibadgujar7822"
+
+#no of words to be removed
+n =5
+
+#print
+print("Initial string",string1)
+
+result = string1[4:]
+
+print("resultant string is",result)
